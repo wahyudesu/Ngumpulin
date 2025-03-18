125bbfee4532c3e5dbd1174b7bf064fa4200d4ee
wahyu ikbal maulana <wahyuikbal777@gmail.com>
2025-03-18 19:22:59 +0700

    Update providers.tsx

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,22 +1,14 @@
-"use client";
+// app/providers.tsx
+'use client'
 
+import { usePathname, useSearchParams } from "next/navigation"
+import { useEffect, Suspense } from "react"
+import { usePostHog } from 'posthog-js/react'
 import { ThemeProvider } from "next-themes";
-import { ReactNode, useEffect, Suspense } from "react";
-import { usePathname, useSearchParams } from "next/navigation";
-import { usePostHog } from 'posthog-js/react';
-import posthog from 'posthog-js';
-import { PostHogProvider as PHProvider } from 'posthog-js/react';
+import { ReactNode } from "react";
 
-export function Providers({ children }: { children: ReactNode }) {
-  return (
-    <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
-      <PostHogProvider>
-        <SuspendedPostHogPageView />
-        {children}
-      </PostHogProvider>
-    </ThemeProvider>
-  );
-}
+import posthog from 'posthog-js'
+import { PostHogProvider as PHProvider } from 'posthog-js/react'
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
@@ -24,41 +16,46 @@ export function PostHogProvider({ children }: { children: React.ReactNode }) {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
       person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
       capture_pageview: false // Disable automatic pageview capture, as we capture manually
-    });
-  }, []);
+    })
+  }, [])
 
   return (
     <PHProvider client={posthog}>
-      {children}
+      <SuspendedPostHogPageView />
+      <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+        {children}
+      </ThemeProvider>
     </PHProvider>
-  );
+  )
 }
 
 function PostHogPageView() {
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
-  const posthog = usePostHog();
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+  const posthog = usePostHog()
 
   // Track pageviews
   useEffect(() => {
     if (pathname && posthog) {
-      let url = window.origin + pathname;
+      let url = window.origin + pathname
       if (searchParams.toString()) {
         url = url + "?" + searchParams.toString();
       }
-      posthog.capture('$pageview', { '$current_url': url });
-    }
-  }, [pathname, searchParams, posthog]);
 
-  return null;
+      posthog.capture('$pageview', { '$current_url': url })
+    }
+  }, [pathname, searchParams, posthog])
+
+  return null
 }
 
 // Wrap PostHogPageView in Suspense to avoid the useSearchParams usage above
 // from de-opting the whole app into client-side rendering
+// See: https://nextjs.org/docs/messages/deopted-into-client-rendering
 function SuspendedPostHogPageView() {
   return (
     <Suspense fallback={null}>
       <PostHogPageView />
     </Suspense>
-  );
+  )
 }
