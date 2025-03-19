24938cdcb88326cbade7e66fea02ba9bc199fb24
Wahyu Ikbal Maulana <wahyuikbal777@gmail.com>
2025-03-20 04:58:29 +0700

    Update layout.tsx

--- src/app/(landingpage)/layout.tsx
+++ src/app/(landingpage)/layout.tsx
@@ -3,6 +3,7 @@ import "@/styles/globals.css";
 import { type Metadata } from "next";
 import Navbar from "./components/navbar";
 import {Footer} from "@/app/(landingpage)/components/footer";
+import { Gabarito } from 'next/font/google'
 
 export const metadata: Metadata = {
   title: "Ngumpulin",
@@ -10,12 +11,19 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
+const gabarito = Gabarito({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-gabarito',
+})
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html
       lang="en"
+      className={`${gabarito.variable}`}
       suppressHydrationWarning
     >
       <body>
